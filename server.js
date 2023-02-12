10958372c577731136f2bd1e38fd8ca082219697
unknown <muofunanyajennifer1@gmail.com>
2023-02-12 23:40:00 +0100

    deleted the server.js file

--- server.js
+++
@@ -1,8 +0,0 @@
-function fruitProcessor(Apples, Oranges){
-    console.log(Apples, Oranges);
-    const juice = `Juice with ${Apples} Apple and ${Oranges} Oranges`;
-    return juice;
-}
-const juiceMix = fruitProcessor(5, 10);
-console.log(juiceMix);
-// console.log(fruitProcessor(5, 10));
